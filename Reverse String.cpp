7d111ccf199340d181be8187a24e66cdbbd47984
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-08-06 09:26:15 +0530

    Reverse String.cpp

--- Reverse String.cpp
+++ Reverse String.cpp
@@ -3,16 +3,12 @@
 using namespace std;
 
 int main() {
-    string str;
+    string str1;
     cout<<"Enter A String: ";
-    cin>>str;
-    int n = str.length();
-
-    for (int i = 0; i < n / 2; i++) {
-        char temp = str[i];
-        str[i] = str[n - i - 1];
-        str[n - i - 1] = temp;
+    cin>>str1;
+    int n = str1.length();
+    for (int i = n; i>=0; i--) {
+        cout<<str1[i];
     }
-    cout << str<<"\n"; 
     return 0;
 }
